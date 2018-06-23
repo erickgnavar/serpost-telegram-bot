98b4a10fc927d032fd9bf438263077b3ac789935
Erick Navarro <erick@navarro.io>
2018-06-22 17:54:28 -0700

    Add info logs and change logger name

--- app.py
+++ app.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import os
 import logging
+import os
 
-from telegram.ext import Updater, CommandHandler
 import serpost
+from telegram.ext import CommandHandler, Updater
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('serpost.telegram.bot')
 
 TELEGRAM_API_TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
 
@@ -29,13 +29,15 @@ You can control me by sending these commands:
 """ + help_message
 
 
-def start(bot, update):
+def start_handler(bot, update):
     bot.sendMessage(update.message.chat_id, text=welcome_message.format(user=update.message.from_user.first_name))
+    logger.info('New user: %s', update.message.from_user.first_name)
 
 
-def track(bot, update, args):
+def track_handler(bot, update, args):
     if len(args) and len(args[0]):
         code = args[0]
+        logger.info('Tracking code: %s', code)
         result = serpost.query_tracking_code(code)
         if result:
             message = '\n'.join(map(format_result, result))
@@ -46,23 +48,22 @@ def track(bot, update, args):
     bot.sendMessage(update.message.chat_id, message)
 
 
-def help(bot, update):
+def help_handler(bot, update):
     bot.sendMessage(update.message.chat_id, help_message)
 
 
-def error(bot, update, error):
-    logger.warn('Update "%s" caused error "%s"' % (update, error))
+def error_handler(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"', update, error)
 
 
 def main():
     updater = Updater(TELEGRAM_API_TOKEN)
 
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CommandHandler('track', track, pass_args=True))
-    updater.dispatcher.add_handler(CommandHandler('help', help))
+    updater.dispatcher.add_handler(CommandHandler('start', start_handler))
+    updater.dispatcher.add_handler(CommandHandler('track', track_handler, pass_args=True))
+    updater.dispatcher.add_handler(CommandHandler('help', help_handler))
 
-    # log all errors
-    updater.dispatcher.add_error_handler(error)
+    updater.dispatcher.add_error_handler(error_handler)
 
     updater.start_polling()
     updater.idle()
